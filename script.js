13e2a723bb933d5252e28b5fa81a8b65b7e3854b
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-02-19 17:48:39 -0600

    Add handvleclickevent function and add eslint

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+const gameBoard = ['', '', '', '', '', '', '', '', '']
 const btn = document.querySelectorAll("button");
+function handleClick(event) {
+    const clickedButton = event.target;
+    const index = clickedButton.getAttribute('data-index');
+    gameBoard[index] = 'X';
+}
 
  btn.forEach(button => {
-    button.addEventListener('click', () => {
-    button.innerHTML = "X"
-    })})
+    button.addEventListener('click', (event) => {
+    handleClick(event)
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].innerHTML = gameBoard[i];
+    }
+})}
 })
+
