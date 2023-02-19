ead7f161222dbc2f28d77fdf438ff623ae83dd78
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-02-19 17:53:59 -0600

    Fix token errors

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-
-const gameBoard = ['', '', '', '', '', '', '', '', '']
-const btn = document.querySelectorAll("button");
-function handleClick(event) {
+document.addEventListener("DOMContentLoaded", () => {
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+  const btn = document.querySelectorAll("button");
+  function handleClick(event) {
     const clickedButton = event.target;
-    const index = clickedButton.getAttribute('data-index');
-    gameBoard[index] = 'X';
-}
+    const index = clickedButton.getAttribute("data-index");
+    gameBoard[index] = "X";
+  }
 
- btn.forEach(button => {
-    button.addEventListener('click', (event) => {
-    handleClick(event)
-    for (let i = 0; i < btn.length; i++) {
+  btn.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      handleClick(event);
+      for (let i = 0; i < btn.length; i++) {
         btn[i].innerHTML = gameBoard[i];
-    }
-})}
-})
-
+      }
+    });
+  });
+});
