450abda9e31e9a7d9af5c762a3d52890d3acc340
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-02-20 11:30:42 -0600

    Add reset button functionality

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
   const btn = document.querySelectorAll("button");
+  let currentPlayer = "X";
+  let winnerDeclared = false;
+  const player1 = document.getElementById("player1Name");
+  const player2 = document.getElementById("player2Name");
+  const reset = document.getElementById("reset");
+
+  reset.addEventListener("click", (event) => {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+  });
 
   function handleClick(event) {
+    if (winnerDeclared) {
+      return;
+    }
+
     const clickedButton = event.target;
     const index = clickedButton.getAttribute("data-index");
     if (gameBoard[index] !== "") {
       return;
     }
 
-    gameBoard[index] = "X";
+    gameBoard[index] = currentPlayer;
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+    player1.classList.toggle("active");
+    player2.classList.toggle("active");
   }
 
   function declareWinner(player) {
@@ -29,22 +46,23 @@ document.addEventListener("DOMContentLoaded", () => {
       return gameBoard[a] === player && gameBoard[b] === player && gameBoard[c] === player;
     });
 
-    return winnerPattern ? "You win!" : null;
+    return winnerPattern ? `${player} win!` : null;
   }
 
   btn.forEach((button) => {
     button.addEventListener("click", (event) => {
       handleClick(event);
       for (let i = 0; i < btn.length; i++) {
-        btn[i].innerHTML = gameBoard[i];
+        if (btn[i].id !== "reset") {
+          btn[i].innerHTML = gameBoard[i];
+        }
       }
       const winner = declareWinner("X") || declareWinner("O");
       const isTie = gameBoard.every((cell) => cell !== "") && !winner;
-      if (winner) {
-        btn.forEach((button) => button.removeEventListener("click", handleClick));
-      }
       if (winner || isTie) {
+        winnerDeclared = true;
         console.log(winner || "It's a tie!");
+        btn.forEach((button) => button.removeEventListener("click", handleClick));
       }
     });
   });
